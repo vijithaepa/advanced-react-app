bc3122921e30c4dc41af354b00063b5b63f19be3
Vijitha Epa <vijithaepa@gmail.com>
2025-01-30 12:28:29 +1100

    intil commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import {Button} from "./components/Button";
+
+function App() {
+  return (
+    // <div className="App">
+
+      <main className='App'>
+        <Button>Hi Vijitha</Button>
+      </main>
+    // </div>
+  );
+}
+
+export default App;
